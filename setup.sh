1ec99c4f6d9b26bb16dc7d242a36e74dc0fc2d13
Ahmed <akatary@mit.edu>
2022-06-08 03:25:49 -0400

    Initial setup files

--- setup.sh
+++ setup.sh
@@ -3,12 +3,10 @@ mkdir Project
 cd Project
 pip3 install virtualenv
 virtualenv -p python3 venv
-source env/bin/activate
 pip3 install django
 pip3 install djangorestframework
 django-admin startproject src 
-cd src
-git clone https://github.com/ATKatary/Crossword.git
+cd src && git clone https://github.com/ATKatary/Crossword.git
 mkdir crossword 
 cp Crossword crossword
 rm -rf Crossword
