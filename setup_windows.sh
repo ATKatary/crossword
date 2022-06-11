86d01c0ec47c07e50dd14e2b14bbd3002867b029
Ahmed <akatary@mit.edu>
2022-06-11 16:34:25 -0400

    Settingup windows and mac instruction

---
+++ setup_windows.sh
@@ -0,0 +1,32 @@
+cd
+mkdir Project
+cd Project
+pip3 install virtualenv
+virtualenv -p python3 venv
+source env/Scripts/activate
+pip3 install django
+pip3 install djangorestframework
+django-admin startproject src 
+cd src 
+pwd
+git clone https://github.com/ATKatary/Crossword.git crossword
+rm -rf ../../temp
+clear
+echo "Now go into Project/src/src/settings.py and add the following to INSTALLED_APPS"
+echo "'rest_framework',"
+echo "'crossword'" 
+read -p "Enter done once you finish: " response
+source ../venv/bin/activate
+python3 manage.py makemigrations
+python3 manage.py migrate
+rm src/urls.py
+touch src/urls.py 
+echo "from django.contrib import admin" >> src/urls.py
+echo "from django.urls import path, include" >> src/urls.py
+echo "urlpatterns = [" >> src/urls.py
+echo "path('admin/', admin.site.urls)," >> src/urls.py
+echo "path('', include('crossword.urls'))" >> src/urls.py
+echo "]" >> src/urls.py
+clear 
+python3 manage.py runserver 
+
